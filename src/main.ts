839ef48ae6678ff4605faa62e63f6f4ad066f170
tatamiya <tamyuji1002@gmail.com>
2022-06-23 22:45:56 +0900

    add: use update function in the main routine

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { DecodedGenre, initializeCcodeConverter } from "./codeconverter";
 
-function crawlingNewBooks() {
+async function crawlingNewBooks() {
   var url: string = "https://www.hanmoto.com/ci/bd/search/hdt/%E6%96%B0%E3%81%97%E3%81%8F%E7%99%BB%E9%8C%B2%E3%81%95%E3%82%8C%E3%81%9F%E6%9C%AC/sdate/today/created/today/order/desc/vw/rss20"
 
 
@@ -15,18 +15,7 @@ function crawlingNewBooks() {
   let ccodeConverter = initializeCcodeConverter();
 
   // Fetch additional information from openBD API and add to book info.
-  bookList.books.forEach(async book => {
-    let isbn = book.isbn;
-    let openbdRes = await requestOpenbdAndParse(isbn);
-    if (openbdRes !== null) {
-      book.addInfoFromOpenbd(openbdRes);
-    }
-
-    let decodedGenre = ccodeConverter.convert(book.ccode);
-    if (decodedGenre !== null) {
-      book.updateGenre(decodedGenre);
-    }
-  });
+  await updateBookInformation(bookList);
 
   bookList.toRows().forEach(row => {
 
